1ea7adcdd4c93cd2f472a7ea334aeb36b98536d9
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-09-21 07:19:05 +0530

    method 2

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import numpy as np
 import cv2 as cv
 import pyautogui
+from time import time
+from PIL import ImageGrab
+
+start = time()
 while True:
-    screenshot = pyautogui.screenshot()
+    screenshot = ImageGrab.grab()
     screenshot = np.array(screenshot)
     screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
-    cv.imshow("Screen Shoot",screenshot)
+    cv.imshow("Screen Shoot", screenshot)
+    print(f'FPS { 1 / (time() - start) }')
+    start = time()
     if cv.waitKey(1) == ord('q'):
         break
 cv.destroyAllWindows()
